03514a39fe19b6a5c26a2dcfae049f306a7ecc46
abhogal <abhogal@LA018017.ds.umcutrecht.nl>
2021-07-21 16:19:15 +0200

    new function

---
+++ plotting/functions/cleanPlot.m
@@ -0,0 +1,8 @@
+function cleanPlot()
+
+set(gca,'xtick',[]); set(gca,'xticklabel',[]); 
+set(gca,'ytick',[]); set(gca,'yticklabel',[]);  
+%set(gca,'XColor', 'none','YColor','none')
+set(gcf,'color','none');
+
+end
