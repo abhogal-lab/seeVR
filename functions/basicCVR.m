48dedac0835897c389cefb358b9ad0425d61f230
abhogal <abhogal@LA018017.ds.umcutrecht.nl>
2023-01-22 10:05:39 +0000

    simplified basic CVR function

--- functions/basicCVR.m
+++ functions/basicCVR.m
@@ -37,10 +37,6 @@ function [delta] = basicCVR(data, mask, base_idx, stim_idx, opts)
 warning('off')
 global opts
 
-if isfield(opts,'smoothmap'); else; opts.smoothmap = 1; end
-
-disp('Normalizing to baseline')
-data = normTimeseries(data,mask,base_idx);
 sBOLD = data; sBOLD(isnan(sBOLD)) = 0;
 
 sBOLD( sBOLD == 0) = NaN;
@@ -50,10 +46,6 @@ base = nanmean(data(:,:,:,base_idx(1):base_idx(2)),4);
 stim = nanmean(data(:,:,:,stim_idx(1):stim_idx(2)),4);
 delta = stim - base; delta(~mask) = NaN;
 
-%spatially smooth map (only done if timeseries is not smoothed already)
-if opts.smoothmap
-delta = smthData(delta , mask, opts);
-end
 delta(delta > 15) = 0; delta(delta < -15) = 0; %remove large BOLD
 delta(isnan(delta)) = 0;
 disp('Saving CVR map')
