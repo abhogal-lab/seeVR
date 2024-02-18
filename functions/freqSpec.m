564a7983db90f1ee53188c6c9379e675a61d3a84
alexb <alexb@Monster>
2024-02-18 11:40:31 +0100

    bugfix linux reg

--- functions/freqSpec.m
+++ functions/freqSpec.m
@@ -1,17 +1,17 @@
 % Copyright (C) Alex A. Bhogal, 2021, University Medical Center Utrecht,
 % a.bhogal@umcutrecht.nl
 % <freqSpec: returns the frequency amplitude (or power) spectrum of input timeseries data >
-% 
+%
 % This program is free software: you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
 % the Free Software Foundation, either version 3 of the License, or
 % (at your option) any later version.
-% 
+%
 % This program is distributed in the hope that it will be useful,
 % but WITHOUT ANY WARRANTY; without even the implied warranty of
 % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 % GNU General Public License for more details.
-% 
+%
 % You should have received a copy of the GNU General Public License
 % along with this program.  If not, see <https://www.gnu.org/licenses/>.
 %
@@ -34,41 +34,44 @@
 % mask
 %
 % freq: the vector of frequency values
-function[ fSpec, meanSpec, freq ] = freqSpec(data, mask, opts)        
+function[ fSpec, meanSpec, freq ] = freqSpec(data, mask, opts)
 
 warning('off')
 global opts
-        if isfield(opts,'powerspec'); else; opts.powerspec = 0; end
-        Fs = 1/opts.TR;
-        %form frequency graphs
-        [voxels,coordinates] = grabTimeseries(data, mask);
-        [x,y,z,N] = size(data);
-        freq = 0:Fs/N:Fs/2;
-        r_xdft = fft(voxels,[],2);           % FFT
-        r_xdft = r_xdft(:,1:N/2+1);           % take half of data
-        if opts.powerspec
-        r_psdx = (1/(Fs*N))*abs(r_xdft).^2;% power
-        else
-        r_psdx = (1/(Fs*N))*abs(r_xdft);% amplitude
-        end
-        r_psdx(2:end-1) = 2*r_psdx(2:end-1);  
-        figure; set(gcf,'color','w');
-        plot(freq,log(mean(r_psdx,1)));
-        if opts.powerspec
-        title('log of power spectrum'); 
-        else
-        title('log of amplitude spectrum');
-        end
-        hold on; 
-        xline(0.01,'k--'); 
-        xline(0.027,'k--');
-        xline(0.073,'k--');
-        xline(0.17,'k--');
-        xline(0.23,'k--'); 
-        hold off
-        meanSpec = mean(r_psdx,1);
-        fSpec = zeros([x*y*z,size(r_psdx,2)]);
-        fSpec(coordinates,:) = r_psdx;
-        fSpec = reshape(fSpec,[x,y,z,size(r_psdx,2)]); 
-        disp('returning spectrum of mask region (NOT logtransformed)')
+if isfield(opts,'powerspec'); else; opts.powerspec = 0; end
+
+Fs = 1/opts.TR;
+%form frequency graphs
+[voxels,coordinates] = grabTimeseries(data, mask);
+[x,y,z,N] = size(data);
+freq = 0:Fs/N:Fs/2;
+r_xdft = fft(voxels,[],2);           % FFT
+r_xdft = r_xdft(:,1:N/2+1);           % take half of data
+if opts.powerspec
+    r_psdx = (1/(Fs*N))*abs(r_xdft).^2;% power
+else
+    r_psdx = (1/(Fs*N))*abs(r_xdft);% amplitude
+end
+r_psdx(2:end-1) = 2*r_psdx(2:end-1);
+figure; set(gcf,'color','w');
+plot(freq,log(mean(r_psdx,1)));
+if opts.powerspec
+    title('log of power spectrum');
+else
+    title('log of amplitude spectrum');
+end
+hold on;
+xline(0.01,'k--');
+xline(0.027,'k--');
+xline(0.073,'k--');
+xline(0.17,'k--');
+xline(0.23,'k--');
+hold off
+meanSpec = mean(r_psdx,1);
+fSpec = zeros([x*y*z,size(r_psdx,2)]);
+fSpec(coordinates,:) = r_psdx;
+fSpec = reshape(fSpec,[x,y,z,size(r_psdx,2)]);
+disp('returning spectrum of mask region (NOT logtransformed)')
+
+
 end
---
+++ registration/adaptElastixTransFile_linux.m
@@ -0,0 +1,47 @@
+function adaptElastixTransFile_linux(filenameIn, filenameOut, parsToAdapt, values)
+%parsToAdapt = InitialTransform = InitialTransformParametersFileName
+%values = C:\/....transformParameters.0.txt
+
+if ~iscell(parsToAdapt)
+    parsToAdapt = {parsToAdapt};
+end
+if ~iscell(values)
+    values = {values};
+end
+% Open file and read content in string
+fileIDorig  = fopen(filenameIn,'r');
+stringFile = 	textscan(fileIDorig,'%s','Delimiter','','endofline','');
+stringFile = stringFile{1}{1};
+fclose(fileIDorig);
+stringFileNew = stringFile;
+for n=1:length(parsToAdapt)
+    % Find location of the parameter to adapt and change 
+    [~,endInd] = regexp(stringFileNew,['(' parsToAdapt{n} ' ']);
+    newlines = strfind(stringFileNew,char(10));
+    indNewLine = find(newlines>endInd,1);
+    stringFileA = stringFileNew(1:endInd-1);
+    stringFileB = stringFileNew(newlines(indNewLine)-1:end); %used to be
+    if isempty(str2num(values{n})) % if it's a character add double quotes
+        stringFileA = [stringFileA ' "' values{n} '"'];
+    else
+        stringFileA = [stringFileA ' ' values{n}];
+    end
+    stringFileNew =[stringFileA stringFileB];
+%     stringFileNew = strrep(stringFileNew,'\','\\');
+    % Save file with adapted parameter
+    fileIDtemp  = fopen(filenameOut,'w');
+    fprintf(fileIDtemp,'%s',stringFileNew);
+    fclose(fileIDtemp);
+end
+
+% replace any possible double quotes (Linux issue)
+fid  = fopen(filenameIn,'rt');
+X = fread(fid);
+fclose(fid);
+X = char(X.');
+Y = strrep(X, '""', '"');
+
+fid2 = fopen(filenameIn,'wt');
+fwrite(fid2,Y);
+fclose(fid2);
+end
