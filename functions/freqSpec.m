e02f0810df29f24fffa4c734e3996c70c1d4daa2
alexb <a.bhogal@umcutrecht.nl>
2025-04-04 15:25:02 +0200

    -updated elastix readme files to include instructions on how to get things working in linux environment - add to bashrc - export libraries - set bin files to be executable

--- functions/freqSpec.m
+++ functions/freqSpec.m
@@ -1,3 +1,21 @@
+% Copyright (C) Alex A. Bhogal, 2025, University Medical Center Utrecht,
+% a.bhogal@umcutrecht.nl
+% <freqSpec: returns power spectrum >
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <https://www.gnu.org/licenses/>.
+%
+% *************************************************************************
 function[ fSpec, vmeanSpec, avgSpec, freq ] = freqSpec(data, mask, opts)
 % freqSpec calculates the frequency spectrum of a 4D timeseries for 
 % voxels defined by a 3D mask.
--- registration/elastix/README.txt
+++ registration/elastix/README.txt
@@ -1,3 +1,9 @@
 *Not really tested for MAC implementation.
 
 *For Linux, path to elastix binaries shuold be added to bashrc
+
+*if you get an error that the libraries cannot be found, you may need to export them explicitly - or add an additional line to bashrc
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/PATH_TO_SEEVR/seeVR-main/seeVR-main/registration/elastix/linux/lib
+
+ALSO - be sure that elastix and transformix can be executed as a program - right click properties and check in the permissions tab
