2a377fd028f3f5627ab9fa65070140eea4d3c811
alexb <a.bhogal@umcutrecht.nl>
2025-04-23 17:48:57 +0200

    streamlined something

---
+++ seeVieweR/definePaths.m
@@ -0,0 +1,10 @@
+function [toolbox,datadir] = definePaths()
+% set paths for seeVieweR
+% 
+% toolbox is the oath to the seeVR toolbox
+%
+% datadir is the path to data is stored
+% 
+toolbox = 'E:\seeVR\';
+datadir = 'G:\4DASL_MRA\DATA\';
+end
