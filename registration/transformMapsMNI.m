63d72c573f8c266832bef8d6055116cd98434a56
alexb <a.bhogal@umcutrecht.nl>
2025-07-25 15:01:00 +0200

    +added CONN toolbox atlas +added filename check to transformMaps functions

--- registration/transformMapsMNI.m
+++ registration/transformMapsMNI.m
@@ -2,27 +2,43 @@ function [] = transformMapsMNI(mapdir, opts)
 global opts
 
 elastixroot = opts.elastixdir;
-       
-%setup OS-dependent paths
+
+% Setup OS-dependent paths
 if ispc
-    elastixrootOS = fullfile(elastixroot,'windows');
+    elastixrootOS = fullfile(elastixroot, 'windows');
 elseif ismac
-    elastixrootOS = fullfile(elastixroot,'mac','bin');
+    elastixrootOS = fullfile(elastixroot, 'mac', 'bin');
 else
-    elastixrootOS = fullfile(elastixroot,'linux','bin');
+    elastixrootOS = fullfile(elastixroot, 'linux', 'bin');
 end
-disp(['path to parameter maps: ',mapdir])
+
+disp(['Path to parameter maps: ', mapdir])
 dirinfo = dir(mapdir);
 
-opts.regOutput = fullfile(mapdir,'regOutput'); mkdir(opts.regOutput);
-disp(['transformed parameter maps will be saved in: ', opts.regOutput])
+opts.regOutput = fullfile(mapdir, 'regOutput');
+mkdir(opts.regOutput);
+disp(['Transformed parameter maps will be saved in: ', opts.regOutput])
 
-%apply Tx
-for kk=1:size(dirinfo,1)
+% Apply transformix only to NIfTI files
+for kk = 1:length(dirinfo)
     if dirinfo(kk).isdir
-    else
-        TxImg = fullfile(dirinfo(kk).folder,dirinfo(kk).name);
-        [~] = transformixReg(TxImg, opts.bsplineTxParamFile, opts.regOutput, elastixrootOS);
+        continue;
+    end
 
-     end
+    [~, ~, ext] = fileparts(dirinfo(kk).name);
+
+    % Check if .nii or .nii.gz (requires special handling for .nii.gz)
+    isNiiGz = endsWith(dirinfo(kk).name, '.nii.gz');
+    isNii = strcmp(ext, '.nii');
+
+    if isNii || isNiiGz
+        TxImg = fullfile(dirinfo(kk).folder, dirinfo(kk).name);
+        try
+            [~] = transformixReg(TxImg, opts.bsplineTxParamFile, opts.regOutput, elastixrootOS);
+        catch ME
+            warning('Failed to run transformixReg on %s: %s', TxImg, ME.message);
+        end
+    else
+        fprintf('Skipping unsupported file: %s\n', dirinfo(kk).name);
+    end
 end
